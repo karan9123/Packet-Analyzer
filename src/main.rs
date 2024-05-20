d805cb1f5651dbe419425d8b3efccf3eeb2a2d5c
karan9123 <karan9123@gmail.com>
2024-05-20 16:23:00 -0400

    updated main.

--- src/main.rs
+++ src/main.rs
@@ -330,7 +330,7 @@ fn print_pcap(block: PcapBlock, filter: Filter) {
 }
 
 
-/*fn main() {
+fn main() {
     let args: Vec<String> = env::args().collect();
     let mut file_name = String::from("test.pcap");
     if args.len() > 1 {
@@ -398,19 +398,19 @@ fn print_pcap(block: PcapBlock, filter: Filter) {
         // the complete PCAP file struct with Pcap Blocks
         print_pcap(pcap_block, my_filter);
     }
-}*/
-
-fn main(){
-    let rcv_socket = UdpSocket::bind("0.0.0.0:4331").expect("could not bind to the receive socket");
-    println!("Listening on: {}", rcv_socket.local_addr().unwrap());
-    let mut buf = [0; 1024];
-
-    let (number_of_bytes, src_addr) = rcv_socket.recv_from(&mut buf)
-        .expect("Didn't receive data");
-    let filled_buf =  buf[..number_of_bytes].to_vec();
-    let ethernet_frame = create_and_return_ether(filled_buf);
-    println!("{}", ethernet_frame);
-    println!("{}", ethernet_frame.packet);
-
 }
+
+// fn main(){
+//     let rcv_socket = UdpSocket::bind("0.0.0.0:4331").expect("could not bind to the receive socket");
+//     println!("Listening on: {}", rcv_socket.local_addr().unwrap());
+//     let mut buf = [0; 1024];
+
+//     let (number_of_bytes, src_addr) = rcv_socket.recv_from(&mut buf)
+//         .expect("Didn't receive data");
+//     let filled_buf =  buf[..number_of_bytes].to_vec();
+//     let ethernet_frame = create_and_return_ether(filled_buf);
+//     println!("{}", ethernet_frame);
+//     println!("{}", ethernet_frame.packet);
+
+// }
 
